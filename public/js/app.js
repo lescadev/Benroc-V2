128581c6ba0cd5cd9814b52084b936984dad402e
Quentin <quentin.senelas@gmail.com>
2019-07-23 11:20:59 +0200

    fix de l'affichage des galeries

--- public/js/app.js
+++ public/js/app.js
@@ -18493,7 +18493,7 @@ return jQuery;
 /* WEBPACK VAR INJECTION */(function(global, module) {var __WEBPACK_AMD_DEFINE_RESULT__;/**
  * @license
  * Lodash <https://lodash.com/>
- * Copyright JS Foundation and other contributors <https://js.foundation/>
+ * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
  * Released under MIT license <https://lodash.com/license>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
  * Copyright Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
@@ -18504,7 +18504,7 @@ return jQuery;
   var undefined;
 
   /** Used as the semantic version number. */
-  var VERSION = '4.17.11';
+  var VERSION = '4.17.15';
 
   /** Used as the size to enable large array optimizations. */
   var LARGE_ARRAY_SIZE = 200;
@@ -21163,16 +21163,10 @@ return jQuery;
         value.forEach(function(subValue) {
           result.add(baseClone(subValue, bitmask, customizer, subValue, value, stack));
         });
-
-        return result;
-      }
-
-      if (isMap(value)) {
+      } else if (isMap(value)) {
         value.forEach(function(subValue, key) {
           result.set(key, baseClone(subValue, bitmask, customizer, key, value, stack));
         });
-
-        return result;
       }
 
       var keysFunc = isFull
@@ -22096,8 +22090,8 @@ return jQuery;
         return;
       }
       baseFor(source, function(srcValue, key) {
+        stack || (stack = new Stack);
         if (isObject(srcValue)) {
-          stack || (stack = new Stack);
           baseMergeDeep(object, source, key, srcIndex, baseMerge, customizer, stack);
         }
         else {
@@ -23914,7 +23908,7 @@ return jQuery;
       return function(number, precision) {
         number = toNumber(number);
         precision = precision == null ? 0 : nativeMin(toInteger(precision), 292);
-        if (precision) {
+        if (precision && nativeIsFinite(number)) {
           // Shift with exponential notation to avoid floating-point issues.
           // See [MDN](https://mdn.io/round#Examples) for more details.
           var pair = (toString(number) + 'e').split('e'),
@@ -25097,7 +25091,7 @@ return jQuery;
     }
 
     /**
-     * Gets the value at `key`, unless `key` is "__proto__".
+     * Gets the value at `key`, unless `key` is "__proto__" or "constructor".
      *
      * @private
      * @param {Object} object The object to query.
@@ -25105,6 +25099,10 @@ return jQuery;
      * @returns {*} Returns the property value.
      */
     function safeGet(object, key) {
+      if (key === 'constructor' && typeof object[key] === 'function') {
+        return;
+      }
+
       if (key == '__proto__') {
         return;
       }
@@ -28905,6 +28903,7 @@ return jQuery;
           }
           if (maxing) {
             // Handle invocations in a tight loop.
+            clearTimeout(timerId);
             timerId = setTimeout(timerExpired, wait);
             return invokeFunc(lastCallTime);
           }
@@ -33291,9 +33290,12 @@ return jQuery;
       , 'g');
 
       // Use a sourceURL for easier debugging.
+      // The sourceURL gets injected into the source that's eval-ed, so be careful
+      // with lookup (in case of e.g. prototype pollution), and strip newlines if any.
+      // A newline wouldn't be a valid sourceURL anyway, and it'd enable code injection.
       var sourceURL = '//# sourceURL=' +
-        ('sourceURL' in options
-          ? options.sourceURL
+        (hasOwnProperty.call(options, 'sourceURL')
+          ? (options.sourceURL + '').replace(/[\r\n]/g, ' ')
           : ('lodash.templateSources[' + (++templateCounter) + ']')
         ) + '\n';
 
@@ -33326,7 +33328,9 @@ return jQuery;
 
       // If `variable` is not specified wrap a with-statement around the generated
       // code to add the data object to the top of the scope chain.
-      var variable = options.variable;
+      // Like with sourceURL, we take care to not check the option's prototype,
+      // as this configuration is a code injection vector.
+      var variable = hasOwnProperty.call(options, 'variable') && options.variable;
       if (!variable) {
         source = 'with (obj) {\n' + source + '\n}\n';
       }
@@ -35531,10 +35535,11 @@ return jQuery;
     baseForOwn(LazyWrapper.prototype, function(func, methodName) {
       var lodashFunc = lodash[methodName];
       if (lodashFunc) {
-        var key = (lodashFunc.name + ''),
-            names = realNames[key] || (realNames[key] = []);
-
-        names.push({ 'name': methodName, 'func': lodashFunc });
+        var key = lodashFunc.name + '';
+        if (!hasOwnProperty.call(realNames, key)) {
+          realNames[key] = [];
+        }
+        realNames[key].push({ 'name': methodName, 'func': lodashFunc });
       }
     });
 
@@ -52611,8 +52616,6 @@ __webpack_require__(/*! ./isotope.pkgd.min.js */ "./resources/js/isotope.pkgd.mi
 
 __webpack_require__(/*! ./barnav */ "./resources/js/barnav.js");
 
-__webpack_require__(/*! ./masonry */ "./resources/js/masonry.js");
-
 __webpack_require__(/*! ./gestionMenu */ "./resources/js/gestionMenu.js");
 
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
@@ -54247,58 +54250,6 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var __WEBPACK_LO
 
 /***/ }),
 
-/***/ "./resources/js/masonry.js":
-/*!*********************************!*\
-  !*** ./resources/js/masonry.js ***!
-  \*********************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-$(document).ready(function () {
-  var $grid = $('.grid').isotope({
-    itemSelector: '.grid-item',
-    masonry: {
-      columnWidth: 200,
-      gutter: 20
-    }
-  });
-  $('#sidebarCollapse').on('click', function () {
-    setTimeout(function () {
-      $grid.isotope('reloadItems').isotope();
-    }, 250);
-  });
-}); //   $('.append-button').on( 'click', function() {
-//       var rand = Math.floor(Math.random() * 6) + 1;
-//       for( var i = 0; i < rand; i++){
-//         // create new item elements
-//         var $items = getItemElement();
-//         // append elements to container
-//         $grid.append( $items )
-//           // add and lay out newly appended elements
-//           .isotope( 'appended', $items );
-//       }
-//   });
-//   // make <div class="grid-item grid-item--width# grid-item--height#" />
-//   function getItemElement() {
-//     var imgs = ['img/photos-dart/2016-04-12-003,medium.2x.1484501038.jpg','img/photos-dart/2016-04-12-004,medium.2x.1484501042.jpg','img/photos-dart/2016-08-01-1,medium.2x.1484500951.jpg'];
-//     var img = imgs[Math.floor(Math.random() * imgs.length)];
-//     var $item = $('<div class="grid-item"><img src="'+img+'"></div>');
-//     // add width and height class
-//     var wRand = Math.random();
-//     var hRand = Math.random();
-//     var widthClass = wRand > 0.85 ? 'grid-item--width3' : wRand > 0.7 ? 'grid-item--width2' : '';
-//     var heightClass = hRand > 0.85 ? 'grid-item--height3' : hRand > 0.5 ? 'grid-item--height2' : '';
-//     $item.addClass( widthClass ).addClass( heightClass );
-//     return $item;
-//   }
-//   // filtering 
-// $('.filters-button-group .button').on('click', function(){
-//   var $class = $(this).attr('data-filter');
-//   $grid.isotope({filter: $class})
-// })
-
-/***/ }),
-
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -54317,8 +54268,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/MAMP/htdocs/benoitrocher-V2/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/benoitrocher-V2/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/Benroc-V2/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/Benroc-V2/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/prestation/galerie.blade.php
+++ resources/views/prestation/galerie.blade.php
@@ -1,17 +1,18 @@
 <div id="galeriecontent">
 
-    <div class="grid">
-        @foreach ($prestationPhotos as $prestationPhoto)
-        <div class="grid-item">
-            <div class="imgSwap">
-                <a href="/img/{{ $prestationPhoto['image'] }}" data-lightbox="prestation"
-                    data-title="{!! $prestationPhoto['description'] !!}"><img
-                        alt="Photographe-Limoges-Prestation-{!! $prestationPhoto['description'] !!}" loading="lazy"
-                        src="/img/{{ $prestationPhoto['image'] }}" /></a>
+    <div class="container">
+        <div class="row">
+            @foreach ($prestationPhotos as $prestationPhoto)
+            <div class="col-lg-2 col-md-4 col-sm-6 resize">
+                <div class="imgSwap">
+                    <a href="/img/{{ $prestationPhoto['image'] }}" data-lightbox="prestation"
+                        data-title="{!! $prestationPhoto['description'] !!}"><img
+                            alt="Photographe-Limoges-Prestation-{!! $prestationPhoto['description'] !!}" loading="lazy"
+                            src="/img/{{ $prestationPhoto['image'] }}" /></a>
+                </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
 
 </div>
-
